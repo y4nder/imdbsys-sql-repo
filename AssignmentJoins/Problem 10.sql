535e66ac71a3b7870a64e806f23205709537d0db
Yander <lorenzolubguban@gmail.com>
2024-09-15 22:45:22 +0800

    minor updates

--- AssignmentJoins/Problem 10.sql
+++ AssignmentJoins/Problem 10.sql
@@ -5,5 +5,5 @@ FROM emp_act ea JOIN employee e ON e.empno = ea.empno
 				JOIN department d ON e.workdept = d.deptno
 				JOIN employee m ON m.empno = d.mgrno
 				JOIN project p ON p.projno = ea.projno
-WHERE ea.emstdate >= '2011-10-15'
+WHERE ea.emstdate BETWEEN '2011-10-01' AND '2011-10-31'
 ORDER BY ea.actno, ea.emstdate;
--- AssignmentJoins/Problem 9.sql
+++ AssignmentJoins/Problem 9.sql
@@ -1,11 +1,9 @@
-SELECT DISTINCT
-		d.deptno,
-		e.lastname, 
-		p.projname, 
-		ea.actno
-FROM department d
-JOIN employee e on e.workdept = d.deptno
-JOIN emp_act ea on ea.empno = e.empno
-JOIN project p on p.respemp = e.empno
-WHERE d.deptno BETWEEN 'A00' AND 'C01'
+USE LUBGUBAN_DB;
+
+SELECT DISTINCT d.deptno, e.lastname, p.projname, ea.actno
+FROM emp_act ea
+JOIN project p on p.projno = ea.projno
+JOIN employee e on e.empno = ea.empno
+JOIN department d on d.deptno = e.workdept
+WHERE e.workdept BETWEEN 'A00' AND 'C01'
 ORDER BY d.deptno, e.lastname, ea.actno;
